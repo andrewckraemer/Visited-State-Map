eb842c5ab0d2f819bbb90a1ee1df62cc984d0963
andrewckraemer <andrew.c.kraemer@gmail.com>
2025-03-13 06:36:41 -0500

    naming update

--- State Map.R
+++ State Map.R
@@ -6,49 +6,40 @@ library(maps)
 options(scipen = 999) ## To disable scientific notation
 
 state <- map_data("state")
-head(state)
-table(world$region)
 
+state$col<-ifelse(state$region=='hawaii','deepskyblue','orange')
 
-worldgovt <- state
-worldgovt$col<-ifelse(worldgovt$region=='hawaii','deepskyblue','orange')
-
-worldgovt$col[worldgovt$region == 'nevada'] <- 'thistle4'
-worldgovt$col[worldgovt$region == 'new mexico'] <- 'thistle4'
-worldgovt$col[worldgovt$region == 'washington'] <- 'deepskyblue'
-worldgovt$col[worldgovt$region == 'virginia'] <- 'deepskyblue'
-worldgovt$col[worldgovt$region == 'idaho'] <- 'deepskyblue'
-worldgovt$col[worldgovt$region == 'oregon'] <- 'deepskyblue'
-worldgovt$col[worldgovt$region == 'montana'] <- 'deepskyblue'
-worldgovt$col[worldgovt$region == 'wyoming'] <- 'deepskyblue'
-worldgovt$col[worldgovt$region == 'michigan'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'arkansas'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'kansas'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'oklahoma'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'missouri'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'indiana'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'north dakota'] <- 'deepskyblue'
-worldgovt$col[worldgovt$region == 'illinois'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'california'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == '?'] <- 'purple'
-worldgovt$col[worldgovt$region == 'wisconsin'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'south dakota'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'minnesota'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'iowa'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'louisiana'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'texas'] <- 'deepskyblue'
-worldgovt$col[worldgovt$region == 'nebraska'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'colorado'] <- 'deeppink2'
-worldgovt$col[worldgovt$region == 'new york'] <- 'mediumseagreen'
-worldgovt$col[worldgovt$region == 'florida'] <- 'mediumseagreen'
-worldgovt$col[worldgovt$region == 'vermont'] <- 'mediumseagreen'
-worldgovt$col[worldgovt$region == 'connecticut'] <- 'mediumseagreen'
-worldgovt$col[worldgovt$region == 'north carolina'] <- 'mediumseagreen'
-
-table(worldgovt$col,worldgovt$region)
-
-## Check for disagreements between the two datasets
-diff <- setdiff(world$region, worldgovt$region)
+state$col[state$region == 'nevada'] <- 'thistle4'
+state$col[state$region == 'new mexico'] <- 'thistle4'
+state$col[state$region == 'washington'] <- 'deepskyblue'
+state$col[state$region == 'virginia'] <- 'deepskyblue'
+state$col[state$region == 'idaho'] <- 'deepskyblue'
+state$col[state$region == 'oregon'] <- 'deepskyblue'
+state$col[state$region == 'montana'] <- 'deepskyblue'
+state$col[state$region == 'wyoming'] <- 'deepskyblue'
+state$col[state$region == 'michigan'] <- 'deeppink2'
+state$col[state$region == 'arkansas'] <- 'deeppink2'
+state$col[state$region == 'kansas'] <- 'deeppink2'
+state$col[state$region == 'oklahoma'] <- 'deeppink2'
+state$col[state$region == 'missouri'] <- 'deeppink2'
+state$col[state$region == 'indiana'] <- 'deeppink2'
+state$col[state$region == 'north dakota'] <- 'deepskyblue'
+state$col[state$region == 'illinois'] <- 'deeppink2'
+state$col[state$region == 'california'] <- 'deeppink2'
+state$col[state$region == '?'] <- 'purple'
+state$col[state$region == 'wisconsin'] <- 'deeppink2'
+state$col[state$region == 'south dakota'] <- 'deeppink2'
+state$col[state$region == 'minnesota'] <- 'deeppink2'
+state$col[state$region == 'iowa'] <- 'deeppink2'
+state$col[state$region == 'louisiana'] <- 'deeppink2'
+state$col[state$region == 'texas'] <- 'deepskyblue'
+state$col[state$region == 'nebraska'] <- 'deeppink2'
+state$col[state$region == 'colorado'] <- 'deeppink2'
+state$col[state$region == 'new york'] <- 'mediumseagreen'
+state$col[state$region == 'florida'] <- 'mediumseagreen'
+state$col[state$region == 'vermont'] <- 'mediumseagreen'
+state$col[state$region == 'connecticut'] <- 'mediumseagreen'
+state$col[state$region == 'north carolina'] <- 'mediumseagreen'
 
 ## Let's ditch many of the unnecessary elements
 plain <- theme(
@@ -62,7 +53,7 @@ plain <- theme(
   plot.title = element_text(hjust = 0.5),
 )
 
-ggplot(data = worldgovt, mapping = aes(x = long, y = lat, group = group)) + 
+ggplot(data = state, mapping = aes(x = long, y = lat, group = group)) + 
   coord_fixed(1.3) +
   geom_polygon(aes(fill = col)) +
   ggtitle("Kraemer-Worthington Adventures") +
